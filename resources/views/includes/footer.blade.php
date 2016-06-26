18162f8286ccfac714e2b335cfd92cd9643769a8
agleis <amg295@cornell.edu>
2016-06-26 16:56:48 -0400

    Deleted everything design-wise

--- resources/views/includes/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="site-footer">
-	<p>Copyright 2015, Coding Connectors Inc.</p>
-</footer>
---
+++ resources/views/layouts/game.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Coding Connection</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12">
+          <img src="../img/logo.png">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
